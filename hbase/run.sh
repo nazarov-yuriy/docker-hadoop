7f21b531169c6ee6eb564412ee8cc6aee127c942
Yuriy Nazarov <nazarov.yuriy.pavlovich@gmail.com>
2020-02-06 15:44:15 +0300

    add hbase

---
+++ hbase/run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+function addProperty() {
+  local path=$1
+  local name=$2
+  local value=$3
+
+  local entry="<property><name>$name</name><value>${value}</value></property>"
+  local escapedEntry=$(echo $entry | sed 's/\//\\\//g')
+  sed -i "/<\/configuration>/ s/.*/${escapedEntry}\n&/" $path
+}
+
+function configure() {
+    local path=$1
+    local module=$2
+    local envPrefix=$3
+
+    local var
+    local value
+
+    echo "Configuring $module"
+    for c in `printenv | perl -sne 'print "$1 " if m/^${envPrefix}_(.+?)=.*/' -- -envPrefix=$envPrefix`; do
+        name=`echo ${c} | perl -pe 's/___/-/g; s/__/@/g; s/_/./g; s/@/_/g;'`
+        var="${envPrefix}_${c}"
+        value=${!var}
+        echo " - Setting $name=$value"
+        addProperty $path $name "$value"
+    done
+}
+
+configure /opt/hbase-$HBASE_VERSION/conf/hbase-site.xml hbase HBASE_CONF
+
+/opt/hbase-$HBASE_VERSION/bin/start-hbase.sh
+tail -f /opt/hbase-$HBASE_VERSION/logs/*
+
